385fcf79063ff9ab6eeb290799fb9040680a283b
Azaz <azaz.ali1706@gmail.com>
2024-09-12 14:48:13 +0530

    initial commit

--- OneDrive/Desktop/shadowspeak/src/app/model/User.ts
+++ OneDrive/Desktop/shadowspeak/src/app/model/User.ts
@@ -15,3 +15,49 @@ const MessageSchema: Schema<Message> = new Schema({
         default: Date.now
     }
 });
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    isVerified: boolean;
+    verifyCodeExpiry: Date;
+    isAcceptingMessage: boolean;
+    message: Message[];
+}
+const UserSchema: Schema<User> = new Schema({
+    username:{
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: [true, 'Email is required'],  
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'Invalid email format']
+    },
+    password:{
+        type: String,
+        required: [true, 'Password is required'],
+    },
+    verifyCode:{
+        type: String,
+        required: [true, 'Verify code is required'],
+    },
+    verifyCodeExpiry:{
+        type: Date,
+        required: [true, 'Verify code expiry is required'],
+    },
+    isVerified:{
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage:{
+        type: Boolean,
+        default: true
+    },
+    message:[MessageSchema]
+})
