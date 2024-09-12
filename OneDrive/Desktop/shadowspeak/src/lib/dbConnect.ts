4e84d68a8ec66f87add60756c6090fd4a6add3e3
Azaz <azaz.ali1706@gmail.com>
2024-09-12 16:55:17 +0530

    db connection handle

--- OneDrive/Desktop/shadowspeak/src/lib/dbConnect.ts
+++ OneDrive/Desktop/shadowspeak/src/lib/dbConnect.ts
@@ -7,5 +7,18 @@ const connection: ConnectionObject = {}
 
 async function dbConnect(): Promise<void> {
     if (connection.isConnected) {
-        return;
+        console.log("Already Connected to database");
+        return
     }
+    try{
+        const db=await mongoose.connect(process.env.MONGO_URI || '', {
+        })
+        console.log("DB connected Successfully");
+        connection.isConnected = db.connections[0].readyState
+    } catch(error) {    
+        console.log("Database connection failed", error);
+        process.exit(1)
+    }
+}
+
+export default dbConnect
